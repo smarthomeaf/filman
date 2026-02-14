2c4cd8dc01f2adbddeecb9175ace5d260553af0e
Smart Home Approval Factor <105226208+smarthomeaf@users.noreply.github.com>
2026-02-14 09:56:52 -0500

    Delete filament_spools directory

--- filament_spools/const.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "filament_spools"
-
-STORAGE_KEY = "filament_spools_store"
-STORAGE_VERSION = 1
-
-# Dispatcher signals for platform updates
-ADD_SIGNAL = "filament_spools_add"
-UPDATE_SIGNAL = "filament_spools_update"
-REMOVE_SIGNAL = "filament_spools_remove"
